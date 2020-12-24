9880a59150e3983d3c5b55a59e7a32bdd5b9df70
root <root@DESKTOP-576V557.localdomain>
2020-12-24 23:36:43 +0200

    NetHunter support

--- build.sh
+++ build.sh
@@ -5,13 +5,13 @@
 
 mkdir -p out
 
-export USE_CCACHE=1
+#export USE_CCACHE=1
 export ARCH=arm64 ARCH_MTK_PLATFORM=mt6755
 export SUBARCH=arm64
-export CROSS_COMPILE=/home/nonta72/linaro49/bin/aarch64-linux-gnu-
+#export CROSS_COMPILE=/home/nonta72/linaro49/bin/aarch64-linux-gnu-
 
-#Defconfig for K6000Plus(and Y6)
-make -C $PWD O=$PWD/out ARCH=arm64 hct6750_66_n_defconfig
+#Defconfig for Y6 with NetHunter support
+make -C $PWD O=$PWD/out ARCH=arm64 ld_defconfig
 #make ARCH=arm64 k6000_plus_defconfig
 
 make -j4 -C $PWD O=$PWD/out ARCH=arm64
